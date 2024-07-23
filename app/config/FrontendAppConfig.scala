9d3720030716aa5cd54f38556f44d8884deadaac
sauravkitukale <173899589+sauravkitukale@users.noreply.github.com>
2024-07-23 11:03:32 +0100

    DDCYLS-5418/added feature flag

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -20,15 +20,19 @@ import com.google.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 @Singleton
-class FrontendAppConfig @Inject() (configuration: Configuration) {
+class FrontendAppConfig @Inject() (configuration: Configuration,servicesConfig: ServicesConfig) {
 
   val host: String    = configuration.get[String]("host")
   val appName: String = configuration.get[String]("appName")
 
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "digital-service-tax-registration-frontend"
+  val dstFrontendBaseUrl: String     = servicesConfig.baseUrl("digital-services-tax-frontend")
+  val dstFrontendRegistrationUrl: String   = dstFrontendBaseUrl + "/digital-services-tax/register/"
+
 
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
@@ -52,4 +56,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Long = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+  lazy val dstNewRegistrationFrontendEnableFlag: Boolean =
+    configuration.getOptional[Boolean]("features.dstNewRegistrationFrontendEnable").getOrElse(false)
+
 }
