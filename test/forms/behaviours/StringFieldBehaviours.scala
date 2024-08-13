f714aac71b8454253ceb4a129a41d24dc0eb6b8f
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2024-08-13 16:48:31 +0100

    DDCYLS-3851 Created function to generate strings based off regular expressions to limit the type of characters created to the English Alphabet only & fixed test

--- test/forms/behaviours/StringFieldBehaviours.scala
+++ test/forms/behaviours/StringFieldBehaviours.scala
@@ -21,14 +21,30 @@ import play.api.data.{Form, FormError}
 
 trait StringFieldBehaviours extends FieldBehaviours {
 
-    def fieldWithMaxLength(form: Form[_],
-                           fieldName: String,
-                           maxLength: Int,
-                           lengthError: FormError): Unit = {
+  def fieldWithMaxLength(form: Form[_],
+                         fieldName: String,
+                         maxLength: Int,
+                         lengthError: FormError): Unit = {
 
     s"not bind strings longer than $maxLength characters" in {
 
       forAll(stringsLongerThan(maxLength) -> "longString") {
+        (string: String) =>
+          val result = form.bind(Map(fieldName -> string)).apply(fieldName)
+          result.errors must contain only lengthError
+      }
+    }
+  }
+
+  def fieldWithMaxLengthGeneratingFromRegex(form: Form[_],
+                                            fieldName: String,
+                                            maxLength: Int,
+                                            regex: String,
+                                            lengthError: FormError): Unit = {
+
+    s"not bind strings longer than $maxLength characters" in {
+
+      forAll(stringsLongerThanGivenRegex(maxLength, regex) -> "longString") {
         (string: String) =>
           val result = form.bind(Map(fieldName -> string)).apply(fieldName)
           result.errors must contain only lengthError
