d53ccb49a1d0e33a5ebf889e841bd81e1e55c2e7
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2024-09-02 08:47:02 +0100

    DDCYLS-3866: Use WireMockSupport trait

--- test/connectors/DSTConnectorSpec.scala
+++ test/connectors/DSTConnectorSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 import com.github.tomakehurst.wiremock.client.WireMock._
@@ -16,15 +32,20 @@ import play.api.http.Status.{NOT_FOUND, OK}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsValue, Json}
 import uk.gov.hmrc.http.HeaderCarrier
-import utils.WiremockServer
+import uk.gov.hmrc.http.test.WireMockSupport
 
-class DSTConnectorSpec extends AnyFreeSpec with WiremockServer with ScalaFutures with IntegrationPatience with ModelGenerators with OptionValues{
+class DSTConnectorSpec extends AnyFreeSpec
+  with ScalaFutures
+  with IntegrationPatience
+  with ModelGenerators
+  with OptionValues
+  with WireMockSupport{
 
   implicit val hc: HeaderCarrier = HeaderCarrier()
 
   lazy val app: Application = new GuiceApplicationBuilder()
     .configure(
-      conf = "microservice.services.digital-services-tax.port" -> mockServer.port()
+      conf = "microservice.services.digital-services-tax.port" -> wireMockPort
     )
     .build()
 
@@ -38,16 +59,7 @@ class DSTConnectorSpec extends AnyFreeSpec with WiremockServer with ScalaFutures
         forAll { (utr: UTR, postcode: Postcode, reg: CompanyRegWrapper) =>
           val escaped = postcode.replaceAll("\\s+", "")
 
-//          stubFor(
-//            get(urlPathEqualTo(s"/lookup-company/$utr/$escaped"))
-//              .willReturn(
-//                aResponse()
-//                  .withStatus(200)
-//                  .withBody(Json.toJson(reg).toString())
-//              )
-//          )
-
-          stubGet(Json.toJson(reg), s"/lookup-company/$utr/$postcode", OK)
+          stubGet(Json.toJson(reg), s"/digital-services-tax/lookup-company/$utr/$escaped", OK)
 
           println(Json.toJson(reg))
 
@@ -65,7 +77,7 @@ class DSTConnectorSpec extends AnyFreeSpec with WiremockServer with ScalaFutures
         val postcode = Arbitrary.arbitrary[Postcode].sample.value
         val escaped = postcode.replaceAll("\\s+", "")
 
-        stubGet(Json.toJson(wrapper), s"/lookup-company/$utr/$escaped", NOT_FOUND)
+        stubGet(Json.toJson(wrapper), s"/digital-services-tax/lookup-company/$utr/$escaped", NOT_FOUND)
 
         val response = connector.lookupCompany(utr, postcode)
         whenReady(response) { res =>
@@ -78,7 +90,7 @@ class DSTConnectorSpec extends AnyFreeSpec with WiremockServer with ScalaFutures
 
 
   private def stubGet(body: JsValue, url: String, status: Int): Any =
-    mockServer.stubFor(
+    wireMockServer.stubFor(
       get(urlPathEqualTo(url))
         .willReturn(
           aResponse()
