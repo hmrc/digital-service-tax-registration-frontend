b0e059bba75d5a368625a1cc65b6eb58c45277d6
Dips <13121210+dips7189@users.noreply.github.com>
2025-06-30 09:46:36 +0100

    DDCYLS-6957 upgrading libraries (#46)
    * DDCYLS-6957 upgrading libraries
    
    * DDCYLS-6957 upgrading plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,22 +2,22 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.0.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "commons-validator"  % "commons-validator"             % "1.8.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "commons-validator"  % "commons-validator"          % "1.8.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "wolfendale"              %% "scalacheck-gen-regexp"   % "0.1.2"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
+    "wolfendale"        %% "scalacheck-gen-regexp"   % "0.1.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
