49cb07c3d84edccb9e2163cd25a9103d7b980274
Oludamilare Matthews <203887119+accenture-dami@users.noreply.github.com>
2025-07-21 11:45:23 +0100

    Platform Upgrades (#50)
    * Platform Upgrades
    
    * Update
    
    * Update
    
    * Update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.16.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
