4a649f282b5a4ee9084a0359ce78c23396ff0fa5
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2026-06-17 10:04:25 +0100

    Upgrade play frontend hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ object AppDependencies {
   private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.8.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "commons-validator"  % "commons-validator"          % "1.10.1"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
