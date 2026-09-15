c3957d3baca72664da85e874b7c0a63c233fcb0e
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2026-09-15 15:30:26 +0100

    DDCYLS-9085: If group page refactor

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,14 +10,14 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "crypto-json-play-30"     % "8.4.0"
+    "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
-    "io.github.wolfendale" %% "scalacheck-gen-regexp" % "1.1.0"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
