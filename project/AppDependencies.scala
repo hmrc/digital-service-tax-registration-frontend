48270e815ed1b1acaa71e9ba3badabefa6f24cd6
Bon Parker <45689313+bonparker@users.noreply.github.com>
2026-09-17 14:40:08 +0100

    DDCYLS-9085: Check if group page rework and read me updates (#85)
    * DDCYLS-9085: Read me corrections
    
    * DDCYLS-9085: If group page refactor
    
    * DDCYLS-9085: Fixing accessibility issue of multiple h1s
    
    * DDCYLS-9085: Updated library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,17 +7,17 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.13.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "crypto-json-play-30"     % "8.4.0"
+    "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
-    "io.github.wolfendale" %% "scalacheck-gen-regexp" % "1.1.0"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
